7af60b71c306b788e598b4cbbaa2f54118b10fa5
Anshul Sharma <anshulbhardwaj0401@gmail.com>
2026-07-03 18:14:14 +0530

    removed edit profile button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./index.css";
-// import { auth, db, storage } from "./firebase";
+import { auth, db, storage } from "./firebase";
 
 import {
   onAuthStateChanged,
